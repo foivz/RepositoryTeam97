aa023d451bcf45495fbea5728f94e0d31aaac4c2
Dajana Jedjud <djedjud@foi.hr>
2015-02-09 15:31:09 +0100

    utrosak_pracenje
    zadnje

---
+++ Mapa/calzedonia_2_pracenje_proizvodnje/T23_Enigma/Compromplus_app/Compromplus_app/formaArtikliPregled.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace Compromplus_app
+{
+    partial class formaArtikliPregled
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formaArtikliPregled));
+            this.lblPretrazivanje = new System.Windows.Forms.Label();
+            this.txtPretrazivanje = new System.Windows.Forms.TextBox();
+            this.lblPretrazivanjeSifra = new System.Windows.Forms.Label();
+            this.txtPretrazivanjeSifra = new System.Windows.Forms.TextBox();
+            this.btnPretrazivanjeSifra = new System.Windows.Forms.Button();
+            this.dgvArtikli = new System.Windows.Forms.DataGridView();
+            this.artiklBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.lblObrisi = new System.Windows.Forms.Label();
+            this.picObrisi = new System.Windows.Forms.PictureBox();
+            this.lblIzmjeni = new System.Windows.Forms.Label();
+            this.picIzmjeni = new System.Windows.Forms.PictureBox();
+            this.lblDodaj = new System.Windows.Forms.Label();
+            this.picDodaj = new System.Windows.Forms.PictureBox();
+            this.lblIzlaz = new System.Windows.Forms.Label();
+            this.picIzlaz = new System.Windows.Forms.PictureBox();
+            this.lblNaslovniText = new System.Windows.Forms.Label();
+            this.picArtikli = new System.Windows.Forms.PictureBox();
+            this.idArtiklDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nazivDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.opisDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bojaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.kolicinaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.klasaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.stavkeDokumentaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArtikli)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.artiklBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picObrisi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picIzmjeni)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDodaj)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picIzlaz)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picArtikli)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblPretrazivanje
+            // 
+            this.lblPretrazivanje.AutoSize = true;
+            this.lblPretrazivanje.Location = new System.Drawing.Point(31, 139);
+            this.lblPretrazivanje.Name = "lblPretrazivanje";
+            this.lblPretrazivanje.Size = new System.Drawing.Size(68, 13);
+            this.lblPretrazivanje.TabIndex = 0;
+            this.lblPretrazivanje.Text = "Pretraživanje";
+            // 
+            // txtPretrazivanje
+            // 
+            this.txtPretrazivanje.Location = new System.Drawing.Point(119, 136);
+            this.txtPretrazivanje.Name = "txtPretrazivanje";
+            this.txtPretrazivanje.Size = new System.Drawing.Size(178, 20);
+            this.txtPretrazivanje.TabIndex = 1;
+            this.txtPretrazivanje.TextChanged += new System.EventHandler(this.txtPretrazivanje_TextChanged);
+            // 
+            // lblPretrazivanjeSifra
+            // 
+            this.lblPretrazivanjeSifra.AutoSize = true;
+            this.lblPretrazivanjeSifra.Location = new System.Drawing.Point(371, 139);
+            this.lblPretrazivanjeSifra.Name = "lblPretrazivanjeSifra";
+            this.lblPretrazivanjeSifra.Size = new System.Drawing.Size(118, 13);
+            this.lblPretrazivanjeSifra.TabIndex = 2;
+            this.lblPretrazivanjeSifra.Text = "Pretraživanje prema šifri";
+            // 
+            // txtPretrazivanjeSifra
+            // 
+            this.txtPretrazivanjeSifra.Location = new System.Drawing.Point(495, 132);
+            this.txtPretrazivanjeSifra.Name = "txtPretrazivanjeSifra";
+            this.txtPretrazivanjeSifra.Size = new System.Drawing.Size(100, 20);
+            this.txtPretrazivanjeSifra.TabIndex = 3;
+            // 
+            // btnPretrazivanjeSifra
+            // 
+            this.btnPretrazivanjeSifra.Location = new System.Drawing.Point(611, 133);
+            this.btnPretrazivanjeSifra.Name = "btnPretrazivanjeSifra";
+            this.btnPretrazivanjeSifra.Size = new System.Drawing.Size(75, 26);
+            this.btnPretrazivanjeSifra.TabIndex = 4;
+            this.btnPretrazivanjeSifra.Text = "Traži";
+            this.btnPretrazivanjeSifra.UseVisualStyleBackColor = true;
+            this.btnPretrazivanjeSifra.Click += new System.EventHandler(this.btnPretrazivanjeSifra_Click);
+            // 
+            // dgvArtikli
+            // 
+            this.dgvArtikli.AllowUserToAddRows = false;
+            this.dgvArtikli.AllowUserToDeleteRows = false;
+            this.dgvArtikli.AutoGenerateColumns = false;
+            this.dgvArtikli.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvArtikli.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idArtiklDataGridViewTextBoxColumn,
+            this.nazivDataGridViewTextBoxColumn,
+            this.opisDataGridViewTextBoxColumn,
+            this.bojaDataGridViewTextBoxColumn,
+            this.kolicinaDataGridViewTextBoxColumn,
+            this.klasaDataGridViewTextBoxColumn,
+            this.stavkeDokumentaDataGridViewTextBoxColumn});
+            this.dgvArtikli.DataSource = this.artiklBindingSource;
+            this.dgvArtikli.Location = new System.Drawing.Point(34, 171);
+            this.dgvArtikli.Name = "dgvArtikli";
+            this.dgvArtikli.Size = new System.Drawing.Size(652, 212);
+            this.dgvArtikli.TabIndex = 5;
+            // 
+            // artiklBindingSource
+            // 
+            this.artiklBindingSource.DataSource = typeof(Compromplus_app.Artikl);
+            // 
+            // lblObrisi
+            // 
+            this.lblObrisi.AutoSize = true;
+            this.lblObrisi.Location = new System.Drawing.Point(514, 104);
+            this.lblObrisi.Name = "lblObrisi";
+            this.lblObrisi.Size = new System.Drawing.Size(58, 13);
+            this.lblObrisi.TabIndex = 17;
+            this.lblObrisi.Text = "Obriši artikl";
+            // 
+            // picObrisi
+            // 
+            this.picObrisi.Image = ((System.Drawing.Image)(resources.GetObject("picObrisi.Image")));
+            this.picObrisi.Location = new System.Drawing.Point(510, 21);
+            this.picObrisi.Name = "picObrisi";
+            this.picObrisi.Size = new System.Drawing.Size(76, 71);
+            this.picObrisi.TabIndex = 16;
+            this.picObrisi.TabStop = false;
+            this.picObrisi.Click += new System.EventHandler(this.picObrisi_Click);
+            // 
+            // lblIzmjeni
+            // 
+            this.lblIzmjeni.AutoSize = true;
+            this.lblIzmjeni.Location = new System.Drawing.Point(384, 104);
+            this.lblIzmjeni.Name = "lblIzmjeni";
+            this.lblIzmjeni.Size = new System.Drawing.Size(64, 13);
+            this.lblIzmjeni.TabIndex = 13;
+            this.lblIzmjeni.Text = "Izmjeni artikl";
+            // 
+            // picIzmjeni
+            // 
+            this.picIzmjeni.Image = ((System.Drawing.Image)(resources.GetObject("picIzmjeni.Image")));
+            this.picIzmjeni.Location = new System.Drawing.Point(388, 25);
+            this.picIzmjeni.Name = "picIzmjeni";
+            this.picIzmjeni.Size = new System.Drawing.Size(76, 71);
+            this.picIzmjeni.TabIndex = 12;
+            this.picIzmjeni.TabStop = false;
+            this.picIzmjeni.Click += new System.EventHandler(this.picIzmjeni_Click);
+            // 
+            // lblDodaj
+            // 
+            this.lblDodaj.AutoSize = true;
+            this.lblDodaj.Location = new System.Drawing.Point(279, 104);
+            this.lblDodaj.Name = "lblDodaj";
+            this.lblDodaj.Size = new System.Drawing.Size(60, 13);
+            this.lblDodaj.TabIndex = 11;
+            this.lblDodaj.Text = "Dodaj artikl";
+            // 
+            // picDodaj
+            // 
+            this.picDodaj.Image = ((System.Drawing.Image)(resources.GetObject("picDodaj.Image")));
+            this.picDodaj.Location = new System.Drawing.Point(273, 21);
+            this.picDodaj.Name = "picDodaj";
+            this.picDodaj.Size = new System.Drawing.Size(76, 71);
+            this.picDodaj.TabIndex = 10;
+            this.picDodaj.TabStop = false;
+            this.picDodaj.Click += new System.EventHandler(this.picDodaj_Click);
+            // 
+            // lblIzlaz
+            // 
+            this.lblIzlaz.AutoSize = true;
+            this.lblIzlaz.Location = new System.Drawing.Point(638, 104);
+            this.lblIzlaz.Name = "lblIzlaz";
+            this.lblIzlaz.Size = new System.Drawing.Size(28, 13);
+            this.lblIzlaz.TabIndex = 19;
+            this.lblIzlaz.Text = "Izlaz";
+            // 
+            // picIzlaz
+            // 
+            this.picIzlaz.Image = ((System.Drawing.Image)(resources.GetObject("picIzlaz.Image")));
+            this.picIzlaz.Location = new System.Drawing.Point(611, 25);
+            this.picIzlaz.Name = "picIzlaz";
+            this.picIzlaz.Size = new System.Drawing.Size(76, 71);
+            this.picIzlaz.TabIndex = 18;
+            this.picIzlaz.TabStop = false;
+            this.picIzlaz.Click += new System.EventHandler(this.picIzlaz_Click);
+            // 
+            // lblNaslovniText
+            // 
+            this.lblNaslovniText.AutoSize = true;
+            this.lblNaslovniText.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.25F);
+            this.lblNaslovniText.Location = new System.Drawing.Point(112, 44);
+            this.lblNaslovniText.Name = "lblNaslovniText";
+            this.lblNaslovniText.Size = new System.Drawing.Size(97, 38);
+            this.lblNaslovniText.TabIndex = 21;
+            this.lblNaslovniText.Text = "Artikli";
+            // 
+            // picArtikli
+            // 
+            this.picArtikli.Image = ((System.Drawing.Image)(resources.GetObject("picArtikli.Image")));
+            this.picArtikli.Location = new System.Drawing.Point(36, 29);
+            this.picArtikli.Name = "picArtikli";
+            this.picArtikli.Size = new System.Drawing.Size(70, 67);
+            this.picArtikli.TabIndex = 22;
+            this.picArtikli.TabStop = false;
+            // 
+            // idArtiklDataGridViewTextBoxColumn
+            // 
+            this.idArtiklDataGridViewTextBoxColumn.DataPropertyName = "IdArtikl";
+            this.idArtiklDataGridViewTextBoxColumn.HeaderText = "Šifra artikla";
+            this.idArtiklDataGridViewTextBoxColumn.Name = "idArtiklDataGridViewTextBoxColumn";
+            // 
+            // nazivDataGridViewTextBoxColumn
+            // 
+            this.nazivDataGridViewTextBoxColumn.DataPropertyName = "naziv";
+            this.nazivDataGridViewTextBoxColumn.HeaderText = "Naziv";
+            this.nazivDataGridViewTextBoxColumn.Name = "nazivDataGridViewTextBoxColumn";
+            // 
+            // opisDataGridViewTextBoxColumn
+            // 
+            this.opisDataGridViewTextBoxColumn.DataPropertyName = "opis";
+            this.opisDataGridViewTextBoxColumn.HeaderText = "Opis";
+            this.opisDataGridViewTextBoxColumn.Name = "opisDataGridViewTextBoxColumn";
+            // 
+            // bojaDataGridViewTextBoxColumn
+            // 
+            this.bojaDataGridViewTextBoxColumn.DataPropertyName = "boja";
+            this.bojaDataGridViewTextBoxColumn.HeaderText = "Boja";
+            this.bojaDataGridViewTextBoxColumn.Name = "bojaDataGridViewTextBoxColumn";
+            // 
+            // kolicinaDataGridViewTextBoxColumn
+            // 
+            this.kolicinaDataGridViewTextBoxColumn.DataPropertyName = "kolicina";
+            this.kolicinaDataGridViewTextBoxColumn.HeaderText = "Količina";
+            this.kolicinaDataGridViewTextBoxColumn.Name = "kolicinaDataGridViewTextBoxColumn";
+            // 
+            // klasaDataGridViewTextBoxColumn
+            // 
+            this.klasaDataGridViewTextBoxColumn.DataPropertyName = "klasa";
+            this.klasaDataGridViewTextBoxColumn.HeaderText = "Klasa";
+            this.klasaDataGridViewTextBoxColumn.Name = "klasaDataGridViewTextBoxColumn";
+            // 
+            // stavkeDokumentaDataGridViewTextBoxColumn
+            // 
+            this.stavkeDokumentaDataGridViewTextBoxColumn.DataPropertyName = "StavkeDokumenta";
+            this.stavkeDokumentaDataGridViewTextBoxColumn.HeaderText = "StavkeDokumenta";
+            this.stavkeDokumentaDataGridViewTextBoxColumn.Name = "stavkeDokumentaDataGridViewTextBoxColumn";
+            this.stavkeDokumentaDataGridViewTextBoxColumn.Visible = false;
+            // 
+            // formaArtikliPregled
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(719, 395);
+            this.Controls.Add(this.picArtikli);
+            this.Controls.Add(this.lblNaslovniText);
+            this.Controls.Add(this.lblIzlaz);
+            this.Controls.Add(this.picIzlaz);
+            this.Controls.Add(this.lblObrisi);
+            this.Controls.Add(this.picObrisi);
+            this.Controls.Add(this.lblIzmjeni);
+            this.Controls.Add(this.picIzmjeni);
+            this.Controls.Add(this.lblDodaj);
+            this.Controls.Add(this.picDodaj);
+            this.Controls.Add(this.dgvArtikli);
+            this.Controls.Add(this.btnPretrazivanjeSifra);
+            this.Controls.Add(this.txtPretrazivanjeSifra);
+            this.Controls.Add(this.lblPretrazivanjeSifra);
+            this.Controls.Add(this.txtPretrazivanje);
+            this.Controls.Add(this.lblPretrazivanje);
+            this.Name = "formaArtikliPregled";
+            this.Text = "Pregled artikala";
+            this.Load += new System.EventHandler(this.formaArtikliPregled_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArtikli)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.artiklBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picObrisi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picIzmjeni)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDodaj)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picIzlaz)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picArtikli)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPretrazivanje;
+        private System.Windows.Forms.TextBox txtPretrazivanje;
+        private System.Windows.Forms.Label lblPretrazivanjeSifra;
+        private System.Windows.Forms.TextBox txtPretrazivanjeSifra;
+        private System.Windows.Forms.Button btnPretrazivanjeSifra;
+        private System.Windows.Forms.DataGridView dgvArtikli;
+        private System.Windows.Forms.BindingSource artiklBindingSource;
+        private System.Windows.Forms.Label lblObrisi;
+        private System.Windows.Forms.PictureBox picObrisi;
+        private System.Windows.Forms.Label lblIzmjeni;
+        private System.Windows.Forms.PictureBox picIzmjeni;
+        private System.Windows.Forms.Label lblDodaj;
+        private System.Windows.Forms.PictureBox picDodaj;
+        private System.Windows.Forms.Label lblIzlaz;
+        private System.Windows.Forms.PictureBox picIzlaz;
+        private System.Windows.Forms.Label lblNaslovniText;
+        private System.Windows.Forms.PictureBox picArtikli;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idArtiklDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nazivDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn opisDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bojaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn kolicinaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn klasaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stavkeDokumentaDataGridViewTextBoxColumn;
+    }
+}
---
+++ Mapa/calzedonia_2_pracenje_proizvodnje/T23_Enigma/Compromplus_app/Compromplus_app/formaArtikliUnos.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Compromplus_app
+{
+    public partial class formaArtikliUnos : Form
+    {
+        private Artikl azuriraj;
+
+        /// <summary>
+        /// Otvaramo artikl koji želimo mjenjati/ažurirati u novoj formi
+        /// </summary>
+        /// <param name="artikl">Artikl koji želimo mjenjati</param>
+        public formaArtikliUnos(Artikl artikl)
+        {
+            InitializeComponent();
+            azuriraj = artikl;
+
+        }
+
+        private void formaArtikliUnos_Load(object sender, EventArgs e)
+        {
+            txtIdArtikl.Focus();
+            if (azuriraj != null)
+            {
+                txtIdArtikl.ReadOnly = true;
+                txtBoja.Text = azuriraj.boja.ToString();
+                txtOpis.Text = azuriraj.opis.ToString();
+                txtNaziv.Text = azuriraj.naziv.ToString();
+                cboKlasa.SelectedItem = azuriraj.klasa.ToString();
+                txtKolicina.Text = azuriraj.kolicina.ToString();
+            }
+           
+
+        }
+
+        private void picSpremi_Click(object sender, EventArgs e)
+        {
+            using (var db = new T23_EnigmaEntities())
+            {
+                if (azuriraj == null)
+                {
+                    //kreiramo novi objekt klase Artikl te ga popunjavamo podacima iz forme
+                    Artikl artikl = new Artikl
+                    {
+                        IdArtikl = int.Parse(txtIdArtikl.Text),
+                        boja = txtBoja.Text,
+                        opis = txtOpis.Text,
+                        naziv = txtNaziv.Text,
+                        klasa = cboKlasa.Text,
+                        kolicina = int.Parse(txtKolicina.Text)
+                    };
+
+                    db.Artikl.Add(artikl);
+                    db.SaveChanges();
+                }
+
+                else //Mjenjamo postojeći artikl   
+                {
+                    db.Artikl.Attach(azuriraj); //registriramo postojeći artikl
+
+                    azuriraj.naziv = txtNaziv.Text;
+                    azuriraj.boja = txtBoja.Text;
+                    azuriraj.opis = txtOpis.Text;
+                    azuriraj.klasa = cboKlasa.Text;
+                    azuriraj.kolicina = int.Parse(txtKolicina.Text);
+                    db.SaveChanges();
+                }
+            }
+            this.Close();
+        }
+
+        private void picIzlaz_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
+    }
+}
